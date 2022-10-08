fe06bba5640c632f444b710e67a850d358d03c67
bolshakovk <bolshakovk1234@gmail.com>
2022-10-08 15:22:22 +0300

    velosiped

--- core/src/java/medical/common/core/CommonApplication.java
+++ core/src/java/medical/common/core/CommonApplication.java
@@ -2,6 +2,7 @@ package medical.common.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class CommonApplication {
--- core/src/test/java/CommonApplicationTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class CommonApplicationTest {
-    @Test
-    public void contextLoads() {
-    }
-}
