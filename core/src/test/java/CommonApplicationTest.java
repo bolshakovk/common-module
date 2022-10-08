2b576c9e481a356721e208666ff2fa7c5626c871
bolshakovk <bolshakovk1234@gmail.com>
2022-10-08 07:18:40 +0300

    fixes

---
+++ core/src/test/java/CommonApplicationTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommonApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
